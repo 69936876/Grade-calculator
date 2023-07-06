7b02255fd5d64a27d03df170353978c86a6179da
ruilin <110581221+69936876@users.noreply.github.com>
2023-07-06 15:49:59 +0800

    Update 成绩.py

--- 成绩.py
+++ 成绩.py
@@ -1,12 +1,16 @@
 score = int(input("请输入成绩："))
 if score>=90:
           print("A")
+          
 elif score>=75:
           print("B")
+          
 elif score>=60:
           print("C")
+          
 elif score<60:
           print("D")
+          
 print("程序运行结束,请重启")
 
 
