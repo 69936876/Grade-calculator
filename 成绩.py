58ddc6af77d565112a3eaa01862bbe591755af7d
ruilin <110581221+69936876@users.noreply.github.com>
2023-07-24 10:37:29 +0800

    Update 成绩.py

--- 成绩.py
+++ 成绩.py
@@ -1,23 +1,30 @@
 print("选择模式")
 print("1、计算成绩等级")
-print("2、关于本程序")
-choice = input("输入你的选择(1/2):")
+print("2、版本号")
+print("3、各地今年中考成绩等级")
+print("4、软件说明")
+choice = input("输入你的选择(1/4):")
 if choice =="1":
-   score=int(input("请输入成绩"))
-elif choice =="2":
-     print('0.1BATA版本')
+    score=int(input("请输入成绩"))
+
+if choice =="2":
+     print('1.0')
      print("程序运行结束，请重启")
+
 elif  choice =="3":
-    print("非法操作,（后续会开放选项）")
+    print('（目前正在搜集各市等级计算方法）')
+
 elif  choice =="4":
-    print("非法操作,（后续会开放选项）")
-if score>=90:
-          print("A")
-elif score>=75:
-          print("B")
-elif score>=60:
-          print("C")
-elif score<60:
-          print("D")
+ print("本软件是用来计算各地中考等级学科等级，有一部份地区中考采用的是主科和体育算入总分，副科算等级")
+ print("每个地方计算方法各不相同，详细可以咨询各市的教育局网站")
+
+elif score >= 90:
+    print("A")
+elif score >= 75:
+    print("B")
+elif score >= 60:
+    print("C")
+elif score < 60:
+    print("D")
 print("程序运行结束,请重启")
 
