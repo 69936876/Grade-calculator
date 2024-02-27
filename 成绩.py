93a1efb9b6b8e1ba1c2757ed77388c9b9e88d9ff
ruilin <110581221+69936876@users.noreply.github.com>
2024-02-27 18:36:20 +0800

    Update 成绩.py

--- 成绩.py
+++ 成绩.py
@@ -2,7 +2,6 @@ print("选择模式")
 print("1、计算成绩等级")
 print("2、版本号")
 print("3、各地今年中考成绩等级")
-print("4、软件说明")
 choice = input("输入你的选择(1/4):")
 if choice =="1":
     score=int(input("请输入成绩"))
@@ -22,9 +21,6 @@ elif  choice =="3":
     print("截止到本地时间为 :", localtime)
     print('目前正在搜集各市等级计算方法')
 
-elif  choice =="4":
- print("本软件是用来计算各地中考等级学科等级，有一部份地区中考采用的是主科和体育算入总分，副科算等级")
- print("每个地方计算方法各不相同，详细可以咨询各市的教育局网站")
 
 elif score >= 90:
     print("A")
